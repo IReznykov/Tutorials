5cd96b076e6dc7a317729c259c114815eaf9c23d
Illya Reznykov <ireznykov@hotmail.com>
2017-10-01 21:28:50 -0700

    Add code source for "Data source and model library" step of tutorial "Create Azure API App with AAD authentication and web jobs"

---
+++ Create Azure API App with AAD authentication and web jobs/Part 01/Data/Database/Goods/Tables/Categories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Goods].[Categories] (
+    [Id]   BIGINT IDENTITY(1, 1) NOT NULL,
+    [Name] NVARCHAR(50) NOT NULL,
+    CONSTRAINT [PK_Goods.Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
